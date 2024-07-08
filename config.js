148b282008c437eafabb4d525eb1aae6d1bbe554
yyktvale <paolozhou1976@gmail.com>
2024-07-08 06:38:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
+  ['+393515533859', true],
   ['xxxxxxxxxx',],
   ['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
